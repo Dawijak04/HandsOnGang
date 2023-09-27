c7a95a01a2302b32f705568eee40e617b81b4ef4
unknown <23371323@studentmail.ul.ie>
2023-09-27 12:44:38 +0100

    wow

---
+++ BooleanFC.java
@@ -0,0 +1,19 @@
+package ISE;
+
+public class BooleanFC {
+	public static void main (String[]args) {
+		int x = 15;
+		int y = 12;
+		boolean result = (x < y) && (x != y );
+		
+		if (result) {
+			System.out.println("Thats true");}
+		else {
+			System.out.println("Thats not possible");}
+	}
+	
+	
+	
+	
+	
+}
